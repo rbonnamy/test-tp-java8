679096840e8168cd6a539fa0a557ed094209575c
ricbon35 <ricbon35@gmail.com>
2017-11-16 09:04:09 +0100

    mise au point

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,12 @@ package java8.ex07;
 import org.junit.Test;
 
 import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -19,13 +23,14 @@ public class DateAndTime_07_Test {
     public void test_date_to_localdate() throws Exception {
 
         // TODO Créer une date Java 1 (12/02/2017)
-        Date date = null;
+        Date date = new Date(117,1,12,0,0,0);
 
         // TODO transformer la date en Instant
+        Instant instant = date.toInstant();
+        ZonedDateTime ofInstant = ZonedDateTime.ofInstant(instant, ZoneId.of(ZoneId.SHORT_IDS.get("ECT"))); // ou ZoneId.of("Europe/Paris")
 
         // TODO transformer la date en LocalDate
-
-        LocalDate result = null;
+        LocalDate result = ofInstant.toLocalDate();
 
         // TODO valoriser les différentes variables afin de rendre le test passant
 
